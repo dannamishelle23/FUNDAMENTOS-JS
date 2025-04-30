1d09e705eadb54f3cb9e1278a05c6adade627f20
dannamishelle23 <dannamishelle.23@gmail.com>
2025-04-30 14:28:30 -0500

    comentarios

--- asincronismo.js
+++ asincronismo.js
@@ -1,8 +1,10 @@
+//Programacion asincrona en JS
 import fetch from 'node-fetch';
 
 let obtenerCat = 'https://api.nekosia.cat/api/v1/images/catgirl';
 
 (async function conectarAPI() {
+    //Manejo de errores con try-catch
     try {
         const respuesta = await (await fetch(obtenerCat)).json();
         console.log(respuesta.image.original.url);
